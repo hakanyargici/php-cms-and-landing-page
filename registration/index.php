2977735e55caf2e529dc4ef550c964d6a5b5ef4f
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-25 01:36:38 +0300

    giriş sayfası hazırlanılıyor. Tema kuruldu ve bazı php script'i yazıldı.

--- registration/index.php
+++ registration/index.php
@@ -8,17 +8,39 @@
 </head>
 <body>
     <div class="login-container">
+        <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
+        if (isset($_POST["submit"])) {
+            $userName = $_POST["username"];
+            $userPassword = $_POST["password"];
+            $errors = array();
+
+            if (empty($userName) or empty($userPassword)){
+                array_push($errors, "Alanlar Boş");
+            if (count($errors) > 0){
+                foreach ($errors as $error) {
+                    echo "$error";
+                }
+            }
+            } else {
+                require_once "../config.php";
+            }
+        }
+        ?>
         <h2>Giriş Yap</h2>
         <form action="index.php" method="post">
             <div class="input-group">
                 <label for="username">Kullanıcı Adı:</label>
-                <input type="text" id="username" name="username" required>
+                <input type="text" id="username" name="username">
             </div>
             <div class="input-group">
                 <label for="password">Şifre:</label>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="password">
             </div>
-            <button type="submit">Giriş Yap</button>
+            <button type="submit" name="submit" id="submit">Giriş Yap</button>
         </form>
     </div>
 </body>
