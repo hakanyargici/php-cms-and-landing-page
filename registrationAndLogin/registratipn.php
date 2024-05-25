b5ddde21024829f834ee8671e4b2ca22a75b4961
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-25 13:21:36 +0300

    title içeriği değiştirildi.

--- registrationAndLogin/registratipn.php
+++ registrationAndLogin/registratipn.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kayıt Ol! Sayfası</title>
+    <title>Kayıt Ol</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
