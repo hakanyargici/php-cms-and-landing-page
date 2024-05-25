8649386ea0b5cc1fc35756e4b54f517fc7a6fe60
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-25 13:23:48 +0300

    login sayfası registration sayfasından alınarak tekrardan düzenlendildi.

---
+++ registrationAndLogin/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giriş Yap!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+        <h2>Giriş Yap!</h2>
+        <form action="index.php" method="post">
+            <div class="input-group">
+                <label for="username">Kullanıcı Adı:</label>
+                <input type="text" id="username" name="username">
+            </div>
+            <div class="input-group">
+                <label for="password">Şifre:</label>
+                <input type="password" id="password" name="password">
+            </div>
+            <button type="submit" name="submit" id="submit">Girş Yap!button>
+            <button type="submit" name="submit" id="submit" href="" >Kayıt Ol!</button>
+        </form>
+    </div>
+</body>
+</html>
