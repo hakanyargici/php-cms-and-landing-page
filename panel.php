c0a9f89fff38b11373141cb1f1121f6bf44cdeb1
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-30 17:26:41 +0300

    panel.php dosyası revize edildi.

--- panel.php
+++ panel.php
@@ -1,31 +1,3 @@
-<?php 
-    if (isset($_POST["login"])) {
-        $username = $_POST["username"];
-        $userPass = $_POST["password"];
-        require_once "Other/config.php";
-
-        // Veritabanı bağlantısını kontrol etme
-        if (!$connectDb) {
-            die("Veritabanı bağlantısı başarısız: " . mysqli_connect_error());
-        }
-
-        $sql = "SELECT * FROM userLogin WHERE userName = '$username'";
-        $result = mysqli_query($connectDb, $sql);
-        $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-        if ($user) {
-            if (password_verify($userPass, $user["userPass"])) {
-                header("Location: Main/main.php");
-                die();
-            } else {
-                echo "<p style='color:red;'>Şifreniz Uyuşmuyor!</p>";
-            }
-        } else {
-            echo "<p style='color:red;'>Kullanıcı Adı Uyuşmuyor!</p>";
-        }
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="tr">
 <head>
@@ -38,21 +10,21 @@
     // Veritabanı bağlantısı
     include_once("Other/config.php");
 
-    // Form gönderildiğinde
     if(isset($_POST['submit'])) {
         $new_link = $_POST['youtube_link'];
         
-        // Yeni linki veritabanına kaydet
         $query = "INSERT INTO ourVideo (iframeLink) VALUES ('$new_link')";
         mysqli_query($connectDb, $query);
     }
     ?>
 
-    <!-- Admin Panel Formu -->
     <form method="post" action="">
         <label for="youtube_link">Yeni YouTube Linki:</label><br>
         <input type="text" id="youtube_link" name="youtube_link"><br>
         <input type="submit" name="submit" value="Linki Güncelle">
     </form>
+
 </body>
 </html>
+
+
