b4c3269e272162efc0271b91785efcbf576244a2
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-31 12:38:50 +0300

    main.php dosyasının adı index olarak güncellendi.

--- index.php
+++ index.php
@@ -2,63 +2,139 @@
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="Style/loginRegister.css">
+    <meta name="author" content="Yusuf Hakan Yargıcı">
+    <title>Product Landing Page</title>
+    <link rel="stylesheet" type="text/css" href="Style/mainPage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="login-container">
-        <?php 
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
+    <header>
+        <div class="navbar"> 
+            <div id="banner">
+                <img src="content/product-landing-page-logo.png" alt="banner">
+            </div>
+            <div id="list">
+                <nav>
+                    <ul>
+                        <li class="menu-elements"><a href="#one">Features</a></li>
+                        <li class="menu-elements"><a href="#two">How It Works</a></li>
+                        <li class="menu-elements"><a href="#three">Pricing</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section class="mainContent">
+        <!--Form / Mail Bilgisi Alma-->
+        <article id="contactMail">
+            <form action="Other/mail.php" method="post">
+                <div id="mails">
+                    <label id="label-email" for="mail">Handcrafted, home-made masterpieces</label>
+                    <input type="email" id="mailBox" name="userMail" required placeholder="Enter Your Email">
+                    <input type="submit" id="submit-button" value="GET STARTED">
+                </div>
+            </form>
+        </article>
+        <!--Features / Sayfa Hakkında-->
+        <article id="one" class="scroll-target"> 
+            <div id="features">
+                <div id="pre-materials" class="featuresSpec">
+                    <figure>
+                        <img src="content/local_fire_department_FILL0_wght400_GRAD0_opsz48.png" alt="materails">
+                    </figure>
+                    <span>
+                        <h1>
+                            Premium Materails
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius veniam sequi labore. Dignissimos id velit facilis cupiditate hic reprehenderit magni recusandae consequatur architecto modi at numquam ea possimus dolores, incidunt illum saepe. Rerum, fugit nulla.
+                        </p>
+                    </span>
+                </div>
+                <div id="fast-shiping" class="featuresSpec">
+                    <figure>
+                    <img src="content/local_shipping_FILL0_wght400_GRAD0_opsz48.png" alt="materails">
+                    </figure>
+                    <span>
+                        <h1>
+                            Fast Shiping
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius veniam sequi labore. Dignissimos id velit facilis cupiditate hic reprehenderit magni recusandae consequatur architecto modi at numquam ea possimus dolores, incidunt illum saepe. Rerum, fugit nulla.
+                        </p>
+                    </span>
+                </div>
+                <div id="qua-assurance" class="featuresSpec">
+                    <figure>
+                        <img src="content/battery_charging_50_FILL0_wght400_GRAD0_opsz48.png" alt="materails">
+                    </figure>
+                    <span>
+                        <h1>
+                            Quality Assurance
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius veniam sequi labore. Dignissimos id velit facilis cupiditate hic reprehenderit magni recusandae consequatur architecto modi at numquam ea possimus dolores, incidunt illum saepe. Rerum, fugit nulla.
+                        </p>
+                    </span>
+                </div>
+            </div>
+        </article>
+        <!--How It Works / Youtube-->
+        <article id="two" class="scroll-target">
 
-            if (isset($_POST["login"])) {
-                $username = $_POST["username"];
-                $userPass = $_POST["password"];
-                require_once "Other/config.php";
-
-                // Veritabanı bağlantısını kontrol etme
-                if (!$connectDb) {
-                    die("Veritabanı bağlantısı başarısız: " . mysqli_connect_error());
-                }
-
-                $sql = "SELECT * FROM userLogin WHERE userName = '$username'";
-                $result = mysqli_query($connectDb, $sql);
-                $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-                if ($user) {
-                    if (password_verify($userPass, $user["userPass"])) {
-                        header("Location: Main/main.php");
-                        die();
-                    } else {
-                        echo "<p style='color:red;'>Şifreniz Uyuşmuyor!</p>";
+            <div id="howItWorks">
+                <?php
+                    error_reporting(E_ALL);
+                    ini_set('display_errors', 1);
+                    include_once("Other/config.php");
+                    // Veritabanı bağlantısını kontrol etme
+                    if (!$connectDb) {
+                        die("Veritabanı bağlantısı başarısız: " . mysqli_connect_error());
                     }
-                } else {
-                    echo "<p style='color:red;'>Kullanıcı Adı Uyuşmuyor!</p>";
-                }
-            }
-        ?>
-        <h2>Login</h2>
-        <form action="index.php" method="post">
-            <div class="input-group">
-                <label for="username">Username:</label>
-                <input type="text" id="username" name="username">
+                    $query = "SELECT iframeLink FROM ourVideo ORDER BY id DESC LIMIT 1";
+                    $result = mysqli_query($connectDb, $query);
+                    $row = mysqli_fetch_array($result);
+                    $youtube_link = $row['iframeLink'];
+                ?>
+
+                <iframe src="<?php echo $youtube_link; ?>" title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
             </div>
-            <div class="input-group">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password">
-            </div>
-            <button type="submit" name="login" id="login">
-                Login
-            </button>
-            <br><br>
-            <a href="Other/registration.php" style="text-decoration: none; color: white;">
-                <button type="button" id="register" >
-                    Register
-                </button>
-            </a>
-        </form>
-    </div>
+        </article>
+        <!--Pricing / Ürünler Ve Fiyat Bilgisi-->
+        <article id="three" class="scroll-target">
+            <div class="box">
+                        <h1>TENOR TROMBONE</h1>
+                        <h3>$600</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis adipisci voluptatum optio animi maiores recusandae!</p>
+                        <button>Select</button>
+                    </div>
+                    <div class="box">
+                        <h1>BASS TROMBONE</h1>
+                        <h3>$900</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis adipisci voluptatum optio animi maiores recusandae!</p>
+                        <button>Select</button>
+                    </div>
+                    <div class="box">
+                        <h1>VALVE TROMBONE</h1>
+                        <h3>$1200</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis adipisci voluptatum optio animi maiores recusandae!</p>
+                        <button>Select</button>
+                    </div>
+                </div>
+        </article>
+    </section>
+    <footer id="copyright">
+        <div class="footer-bar">
+            <p>Copyright 2016, Original Trombones</p>
+            <p>
+                <a href="Other/login.php">Login For Worker</a>
+            </p>
+        </div>
+    </footer>
 </body>
 </html>
