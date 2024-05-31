fb513285d0ba5b258bd09f5feb1487dd593c2742
Yusuf Hakan Yargıcı <hakanyargici@outlook.com>
2024-05-31 16:33:02 +0300

    İlgisiz dosyalar sistemden kaldırıldı.

--- adminInterface/adminMain.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "Giriş Başarılı!";
-?>
--- adminInterface/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $userConnect = new mysqli("localhost","hakanyargici","1234","originalTrombones");
-    $userConnect -> set_charset("utf8");
-?>
